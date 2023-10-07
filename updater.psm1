4cf70dad813449349c8b0955e4a765fc12f277a5
UltraAlex0 <104384457+UltraAlex00@users.noreply.github.com>
2023-10-07 19:00:38 +0200

    Update updater.psm1

--- updater.psm1
+++ updater.psm1
@@ -1,11 +1,24 @@
 class Updater {
 
-    static [Version]$Version = Import-PowerShellDataFile "$([Environment]::CurrentDirectory)\PSDllCompiler.psd1").ModuleVersion
+    static [Version]$CurrentVersion = (Import-PowerShellDataFile "$([Environment]::CurrentDirectory)\PSDllCompiler.psd1").ModuleVersion
+
+    static [Version]$NewestVersion = (Find-Module PSDllCompiler).Version
 
     static [bool] UpdateAvailable() {
     
-        if ((Find-Module PSDllCompiler).Version -gt [Updater]::Version) {return $true}
+        if ([Updater]::NewestVersion -gt [Updater]::CurrentVersion) {return $true}
 
         return $false
+    }
+    static [string] GetReleaseNotes() {
+        
+        $response = Invoke-RestMethod -Uri "https://www.powershellgallery.com/packages/PSDllCompiler"
+
+        $index = ($response.Split("`n") | Select-String @("Release Notes", "FileList ")).LineNumber
+
+        $ReleaseNotes = [string]($response.Split("`n")[$index[0]..($index[1] - 3)]).Replace("<br />", "`n")
+        $ReleaseNotes = $ReleaseNotes.Replace('<p class="content-collapse-in">', "").Replace("</p>", "").Replace("</div>", "").Trim()
+
+        return $ReleaseNotes
     }
 }
